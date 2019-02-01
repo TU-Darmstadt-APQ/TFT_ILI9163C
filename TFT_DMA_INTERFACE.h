88581712d761268d8b4b8c8cf765b9c75ef2e297
ThomasElRubio <thomas.sattelmaier@gmail.com>
2019-02-01 18:59:26 +0100

    Upgraded so updates which are bigger than the buffer are able to be rendered and transfered to the display.

--- TFT_DMA_INTERFACE.h
+++ TFT_DMA_INTERFACE.h
@@ -16,6 +16,11 @@ class TftDmaInterface {
 		this->transmitData.destination((volatile uint32_t &) SPI0_PUSHR);
 		this->transmitData.triggerAtHardwareEvent(DMAMUX_SOURCE_SPI0_TX);
 		this->transmitData.disableOnCompletion();
+		
+		this->backupChannel.disable();
+		this->backupChannel.destination((volatile uint32_t &) SPI0_PUSHR);
+		this->backupChannel.triggerAtCompletionOf(this->transmitData);
+		this->backupChannel.disableOnCompletion();
 	};
 	
 	void setSpiForTFT(){
@@ -32,44 +37,85 @@ class TftDmaInterface {
 	};
 	
 	// This functions is called after the data that will be transmitted is written into the buffer
-	void triggerDMA(){
-		if(counter>0){
-			//if(counter>10000) Serial.println(counter);
-			SPI0_SR |= (1<<28);
-			if(useBackupBuffer){
-				this->backupBuffer[this->counter-1] |= 1 << 27; 				//EOQ bit in SPI0_PUSHR		
-				this->transmitData.sourceBuffer(this->backupBuffer,counter*4);
-				this->transmitData.enable();
-				this->useBackupBuffer = false;
-			}
-			else{
-				this->buffer[this->counter-1] |= 1 << 27; 				//EOQ bit in SPI0_PUSHR		
-				this->transmitData.sourceBuffer(this->buffer,counter*4);
-				this->transmitData.enable();
-				this->useBackupBuffer = true;
+	void triggerDMA()__attribute__((always_inline)){
+		if(hugeRender){
+			this->hugePicture();
+		}
+		else{
+			if(counter>0){
+				//if(counter>10000) Serial.println(counter);
+				SPI0_SR |= (1<<28);
+				if(this->useBackupBuffer){
+					this->backupBuffer[this->counter-1] |= 1 << 27; 				//EOQ bit in SPI0_PUSHR		
+					this->transmitData.sourceBuffer(this->backupBuffer,counter*4);
+					this->transmitData.enable();
+					this->useBackupBuffer = false;
+				}
+				else{
+					this->buffer[this->counter-1] |= 1 << 27; 				//EOQ bit in SPI0_PUSHR		
+					this->transmitData.sourceBuffer(this->buffer,counter*4);
+					this->transmitData.enable();
+					this->useBackupBuffer = true;
+				}
 			}
 		}
 	};
 	
+	void hugePicture()__attribute__((always_inline)){
+		SPI0_SR |= (1<<28);
+		if(this->useBackupBuffer){
+			this->transmitData.sourceBuffer(this->buffer,27500*4);
+			this->backupBuffer[this->counter-1] |= 1 << 27;
+			this->backupChannel.sourceBuffer(this->backupBuffer,counter*4);
+			this->transmitData.enable();
+			this->backupChannel.enable();
+			this->useBackupBuffer = !this->useBackupBuffer;
+		}
+		else{
+			this->transmitData.sourceBuffer(this->backupBuffer,27500*4);
+			this->buffer[this->counter-1] |= 1 << 27;
+			this->backupChannel.sourceBuffer(this->buffer,counter*4);
+			
+			this->transmitData.enable();
+			this->backupChannel.enable();
+			this->useBackupBuffer = !this->useBackupBuffer;
+		}
+		this->hugeRender = !this->hugeRender;
+	};
+	
 	void writeToBuffer(uint32_t d)__attribute__((always_inline)){
-		if(useBackupBuffer){
+		if(this->useBackupBuffer){
 			this->backupBuffer[this->counter] = d;
-			this->counter++;
+			if(counter <27500) this->counter++;
+			else{
+				Serial.println("inbackup");
+				counter = 0;
+				useBackupBuffer = !useBackupBuffer;
+				hugeRender = true;
+			}
 		}
 		else{
 			this->buffer[this->counter] = d;
-			this->counter++;
+			if(counter <27500) this->counter++;
+			else{
+				Serial.println("inbuffer");
+				counter = 0;
+				useBackupBuffer = !useBackupBuffer;
+				hugeRender = true;
+			}
 		}
 		
 	};
 	protected:
 		int32_t counter = 0;
 		bool useBackupBuffer = false;
+		bool hugeRender = false;
 		DMAChannel transmitData = DMAChannel(4);
+		DMAChannel backupChannel = DMAChannel(5);
 		const uint32_t SPI_RESUME_TRANSACTION = 0b1 << 28; 
 		const uint32_t SPI_END_TRANSACTION = 0b11 << 27;
-		uint32_t buffer[27500];
-		uint32_t backupBuffer[27500];
+		uint32_t buffer[27000];
+		uint32_t backupBuffer[27000];
 		
 };
 
